c975def3d7fe5721c1c5944e93bbc44ca82c6e4e
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-07-01 06:27:58 +0300

    Theme colors

--- src/AwesomeProject/src/theme/colors.tsx
+++ src/AwesomeProject/src/theme/colors.tsx
@@ -1,14 +1,19 @@
 import {
   MD3DarkTheme as DarkTheme,
-  MD3LightTheme as LightTheme
+  MD3LightTheme as LightTheme,
 } from 'react-native-paper';
 
 const lightColors = {
   ...LightTheme.colors,
 
-  appBg: '#ffffff',
-  appBarBg: '#cccccc',
-  normalText: '#000000'
+  appBg: '#DEDEDE',
+  appBarBg: '#F4F4F4',
+  normalText: '#4E4E4E',
+  searchBarBg: '#F0F0F0',
+
+  noteBg: '#FFFFFF',
+  cardBg: '#EAEAEA',
+  smallCardBg: '#FFFFFF',
 };
 
 type themeColors = typeof lightColors;
@@ -16,9 +21,14 @@ type themeColors = typeof lightColors;
 const darkColors: themeColors = {
   ...DarkTheme.colors,
 
-  appBg: '#000000',
-  appBarBg: '#333',
-  normalText: '#ffffff'
+  appBg: '#222222',
+  appBarBg: '#000000',
+  normalText: '#dddddd',
+  searchBarBg: '#555555',
+
+  noteBg: '#000000',
+  cardBg: '#2F2F2F',
+  smallCardBg: '#000000',
 };
 
-export { darkColors, lightColors };
+export {darkColors, lightColors};
--- src/AwesomeProject/z-tasks.md
+++ src/AwesomeProject/z-tasks.md
@@ -1,7 +1,19 @@
 # Tasks:
 
-- [ ]: Components: CircleIcon
-- [ ]: Components: appBar
-- [ ]: Side Drawer
-- [ ]: AppBar: hasBackButton
-- [ ]: 2 more componenets screens
+- [x]: Figure out colors
+- [ ]: Theme Cicle Icon
+- [ ]: Download AppBar SVGs
+- [ ]: use the first svg (Probably using React Native svg)
+- [ ]: make svgs visible (maybe using ts config)
+- [ ]: Add all screens (Empty)
+- [ ]: Figure out components
+- Screens (Shape only):
+  - [ ]: Screen 1
+  - [ ]: Screen 2
+  - [ ]: Screen 3
+  - [ ]: Screen 4
+  - [ ]: Screen 5
+- [ ]: now to DataBase
+- [ ]: Rename Project
+- [ ]: Change the Project Icon
+- [ ]: generate APK
