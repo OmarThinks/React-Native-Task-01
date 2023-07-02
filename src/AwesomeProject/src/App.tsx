84b52104f9901eae18cf73832c0dc7815aeb140f
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-07-03 02:27:19 +0300

    DbContext: provided

--- src/AwesomeProject/src/App.tsx
+++ src/AwesomeProject/src/App.tsx
@@ -1,18 +1,18 @@
+import {DBContext} from '@contexts';
 import {RootStack} from '@navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {store, themeSelector} from '@redux';
 import {darkTheme, lightTheme} from '@theme';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {PaperProvider} from 'react-native-paper';
-import {Provider as ReduxProvider, useSelector} from 'react-redux';
-import {getModelItems, getDBConnection} from './storage';
-import {DBContext} from '@contexts';
 import {SQLiteDatabase} from 'react-native-sqlite-storage';
+import {Provider as ReduxProvider, useSelector} from 'react-redux';
+import {getDBConnection, getModelItems} from './storage';
 
 const dbStaff = async () => {
-  const db = await getDBConnection();
-  const models = await getModelItems(db);
-  console.log(models);
+  // const db = await getDBConnection();
+  // const models = await getModelItems(db);
+  // console.log(models);
 };
 const AppWithoutRedux = () => {
   const theme = useSelector(themeSelector);
@@ -28,6 +28,16 @@ const AppWithoutRedux = () => {
 
 const App = () => {
   const [db, setDb] = useState<SQLiteDatabase | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const _db = await getDBConnection();
+      setDb(_db);
+    };
+    init();
+  }, []);
+
+  console.log(db);
 
   return (
     <DBContext.Provider value={db}>
