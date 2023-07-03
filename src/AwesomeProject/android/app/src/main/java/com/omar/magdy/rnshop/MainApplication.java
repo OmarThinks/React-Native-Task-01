556a19c442d336295ef7b0398b8e8e9a7cfab8cd
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-07-03 21:12:20 +0300

    Renamed project to "React Native Shop"

--- src/AwesomeProject/android/app/src/main/java/com/omar/magdy/rnshop/MainApplication.java
+++ src/AwesomeProject/android/app/src/main/java/com/omar/magdy/rnshop/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.omar.magdy.rnshop;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- src/AwesomeProject/android/app/src/release/java/com/omar/magdy/rnshop/ReactNativeFlipper.java
+++ src/AwesomeProject/android/app/src/release/java/com/omar/magdy/rnshop/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.awesomeproject;
+package com.omar.magdy.rnshop;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
